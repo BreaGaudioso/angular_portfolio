5f21a08662dac76b419c14d0740610096092529c
Brea Gaudioso <breagaudioso@gmail.com>
2015-11-03 19:36:09 -0800

    cleaned syntax

--- app/js/app.js
+++ app/js/app.js
@@ -6,13 +6,13 @@ app.config(function($routeProvider, $locationProvider){
     templateUrl: 'partials/ProjectsController.html',
     controller: 'ProjectsController'
   })
-    .when('/bio', {
-      templateUrl: 'partials/BioController.html',
-      controller: 'BioController'
-    })
-    .when('/resume', {
-      templateUrl: 'partials/ResumeController.html',
-      controller: 'ResumeController'
-    })
-    $locationProvider.html5Mode(true);
+  .when('/bio', {
+    templateUrl: 'partials/BioController.html',
+    controller: 'BioController'
+  })
+  .when('/resume', {
+    templateUrl: 'partials/ResumeController.html',
+    controller: 'ResumeController'
+  })
+  $locationProvider.html5Mode(true);
 });
